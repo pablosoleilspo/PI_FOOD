4db576dab7c36b1daf236deab56220bd0985c983
pablo vasquez <105767524+pablosoleilspo@users.noreply.github.com>
2023-02-08 15:12:25 -0500

    esto es

--- api/src/app.js
+++ api/src/app.js
@@ -23,6 +23,7 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
+server.use(cors())
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
